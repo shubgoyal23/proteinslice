2e15f41d8d67255d239e959e574aee151f444a83
shubgoyal23 <shubgoyal1998@gmail.com>
2024-03-08 19:01:42 +0530

    for check

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import { connectDb } from "./db/conectDb.js";
 dotenv.config();
 const port = Number(process.env.PORT) + 1;
 
-console.log("this", process.env.RAZORPAY_KEY_ID)
-console.log("this", process.env.PORT)
-
 connectDb().then(() => {
    app.listen(port, () => {
       console.log(`server starte at http://localhost:${port}`);
