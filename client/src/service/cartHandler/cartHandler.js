8c2f07515a8110bbac731b7ded41273c14dab60d
shubham g <shubhamgoyal.dev@gmail.com>
2024-11-26 22:54:32 +0530

    removed frontend files

--- client/src/service/cartHandler/cartHandler.js
+++
@@ -1,7 +0,0 @@
-export function saveCartToStorage(cart) {
-  window.localStorage.setItem("cart", JSON.stringify(cart));
-}
-export function getCartFromStorage() {
-  const cart = window.localStorage.getItem("cart");
-  return JSON.parse(cart) || [];
-}
--- client/src/service/conf/conf.js
+++
@@ -1,6 +0,0 @@
-const conf = {
-  URL: import.meta.env.VITE_BASE_URL || "",
-  RECAPTCHA: import.meta.env.VITE_PS_RECAPTCHA_SITE_KEY,
-};
-
-export default conf;
